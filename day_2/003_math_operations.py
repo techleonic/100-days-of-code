8e15756aadbbd50ccc50dab50c3b694718558bcd
techleonic <techleonic@gmail.com>
2024-05-09 09:06:23 -0600

    day2

---
+++ day_2/003_math_operations.py
@@ -0,0 +1,19 @@
+#math operations#
+3 + 5
+3 - 4
+3 * 2
+6 / 3
+2 ** 2
+
+#PEMDAS
+"""
+() parentesis
+** exponential
+*  multiplicacion
+/ division
++  adddition
+- sustraction
+"""
+
+print(3 * 3 + 3 / 3 - 3)
+
