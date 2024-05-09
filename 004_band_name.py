d7d29db787b9a7e84835d029f89a7d183dc8b08a
techleonic <techleonic@gmail.com>
2024-05-08 19:42:12 -0600

    band name generator day1

--- 004_band_name.py
+++ 004_band_name.py
@@ -2,4 +2,4 @@ print("Welcome to the band name generator")
 pet = input("what's yor pet name? \n")
 city = input("what's yor city name? \n")
 
-print(f"The name of your band will be: {pet}{city}")
+print(f"The name of your band will be: {city} {pet}s")
