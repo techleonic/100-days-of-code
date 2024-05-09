023ec5ea41b20c7d5b5ea8f9b5d9d196d57e8088
techleonic <techleonic@gmail.com>
2024-05-09 08:32:21 -0600

    day_2

---
+++ day_2/001_datatype.py
@@ -0,0 +1,36 @@
+# DATA TYPE#
+
+#string
+print("hello"[0]) #subscripting
+
+print("hello" + "world")
+
+#integer
+print(123 + 345)
+
+#large number _
+n = 123_456_789
+print(n)
+
+#float
+pi = 3.14169
+
+#boolean
+True
+False
+
+#type function
+print(type(pi))
+print(type(n))
+
+#type convertion type casting
+n = str(n)
+pi = str(pi)
+print("N: " + n)
+print("N: " + pi)
+
+numbers =  input("")
+n1 = int (numbers[0])
+n2 = int (numbers[1])
+
+print( n1 + n2)
