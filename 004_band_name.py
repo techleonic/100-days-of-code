366863696bef7d60620820b95452b961efe38d4d
techleonic <techleonic@gmail.com>
2024-05-08 19:39:14 -0600

    band name proyect day1

---
+++ 004_band_name.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+pet = input("what's yor pet name? \n")
+city = input("what's yor city name? \n")
+
+print(f"The name of your band will be: {pet}{city}")
---
+++ day_1/003_variable.py
@@ -0,0 +1,16 @@
+name = "jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+#siwtch names around#
+a = input()
+b = input()
+
+c = a
+b = a
+a = c
+
+print(a)
+print(b)
