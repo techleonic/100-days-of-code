42a20344f7d41f27ee7f4d32c01507b1d7fb02be
techleonic <techleonic@gmail.com>
2024-05-29 14:44:09 -0600

    turtle square

---
+++ day_18/TutleSquare.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import Turtle,Screen
+
+tutle =  Turtle()
+tutle.color("red")
+position = turtle.pos()
+
+for i in range(4):
+    turtle.right(90)
+    turtle.forward(90)
+
+
+screen = Screen()
+screen.exitonclick()
+
+
+
