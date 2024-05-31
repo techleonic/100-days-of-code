e4010e62e3bfcaedc247daf3ca4511623d0451af
techleonic <techleonic@gmail.com>
2024-05-31 15:10:58 -0600

    draw a dashed line

---
+++ day_18/draw_dashed.py
@@ -0,0 +1,14 @@
+from turtle import Turtle,Screen
+
+my_turtle = Turtle()
+for _ in range(10):
+    my_turtle.forward(10)
+    my_turtle.penup()
+    my_turtle.forward(10)
+    my_turtle.pendown()
+
+
+
+
+screen =  Screen()
+screen.exitonclick()
