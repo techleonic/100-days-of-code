87723a41c8ffd1d527de3f2ebf8b73f3e26b6fd3
techleonic <techleonic@gmail.com>
2024-05-31 18:07:30 -0600

    tuple

--- day_18/random_walk.py
+++ day_18/random_walk.py
@@ -1,2 +1,17 @@
+import turtle
 from turtle import  Turtle,Screen
+import random
 my_turtle = Turtle ()
+
+directions= [0,90,180,270]
+colors = ["royal blue","lime","light coral","saddle brown","medium slate blue"]
+my_turtle.pensize(15)
+my_turtle.speed(20)
+for _ in range(100):
+    my_turtle.color(random.choice(colors))
+    my_turtle.forward(50)
+    my_turtle.setheading(random.choice(directions))
+
+
+screen = Screen()
+screen.exitonclick()
---
+++ day_18/tuple.py
@@ -0,0 +1,8 @@
+my_tuple = (1, 3, 8)
+print(my_tuple)
+# my_tuple[2]=1  TUPLES CAN NOT BE REASSIGN
+
+#convert a tuple to a list
+list_from_tuple = list(my_tuple)
+list_from_tuple[2]=1
+print(list_from_tuple)
