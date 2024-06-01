29e0e5f41e36ee922b0d3c171e83e628676002da
techleonic <techleonic@gmail.com>
2024-05-31 18:21:19 -0600

    random walk random color

--- day_18/random_walk.py
+++ day_18/random_walk.py
@@ -2,13 +2,24 @@ import turtle
 from turtle import  Turtle,Screen
 import random
 my_turtle = Turtle ()
+turtle.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return  random_color
+
 
 directions= [0,90,180,270]
-colors = ["royal blue","lime","light coral","saddle brown","medium slate blue"]
+
+
 my_turtle.pensize(15)
 my_turtle.speed(20)
+
+
 for _ in range(100):
-    my_turtle.color(random.choice(colors))
+    my_turtle.color(random_color())
     my_turtle.forward(50)
     my_turtle.setheading(random.choice(directions))
 
