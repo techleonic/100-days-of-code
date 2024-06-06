ddd52eb969606e60d2c0339d245c44351638b4df
techleonic <techleonic@gmail.com>
2024-06-06 16:33:20 -0600

    drawing dots 2

--- day_18/drawind_dots_2.py
+++ day_18/drawind_dots_2.py
@@ -4,12 +4,13 @@ import random
 my_turtle = turtle_module.Turtle()
 turtle_module.colormode(255)
 list_colors = [(252, 138, 52), (49, 171, 90), (120, 185, 140),  (249, 75, 110)]
-
+my_turtle.penup()
 my_turtle.setheading(225)
 my_turtle.forward(300)
 my_turtle.setheading(0)
 number_of_dots = 100
 my_turtle.speed("fastest")
+
 
 for dot_count in range(1, number_of_dots+1):
     my_turtle.dot(20, random.choice(list_colors))
