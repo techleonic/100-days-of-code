c0edf594b473e7ade5145fed74beb5fbdb2122eb
techleonic <techleonic@gmail.com>
2024-06-20 20:36:12 -0600

    food managment

---
+++ day_21/snake_game.py
@@ -0,0 +1,33 @@
+from turtle import Screen
+import time
+from food import Food
+from snake import Snake
+
+screen =  Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+
+while True :
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detec collision with food
+    if  snake.segments[-1].distance(food) < 15 :
+        food.refersh()
+
+
+
+screen.exitonclick()
