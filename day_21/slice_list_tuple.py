14828944d1b19336a79ba3c2fea1d4d760835577
techleonic <techleonic@gmail.com>
2024-06-21 20:20:35 -0600

    slice

---
+++ day_21/slice_list_tuple.py
@@ -0,0 +1,13 @@
+piano_keys = ["a","b","c","d","e","f","g"]
+
+print(piano_keys[2:5])
+
+print(piano_keys[2:]) #from slice 2 to the end
+
+print(piano_keys[:5]) # from the start to the last slice
+
+print(piano_keys[2:5:2]) # increasing 2 from slice 2 to 5
+
+print(piano_keys[::2]) #print every 2 slices
+
+print(piano_keys[::-1]) # revers a list
--- day_21/snake_game.py
+++ day_21/snake_game.py
@@ -34,15 +34,13 @@ while game_is_one :
         score.update_score()
 
     #DETEC COLITION WITH WALL
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_one = False
         score.game_over()
 
     #detec head collision eith tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_one = False
             score.game_over()
 
