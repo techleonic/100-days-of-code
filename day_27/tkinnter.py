560b6cd5bbe89a56a1e4acc1dc163627f42be35e
techleonic <techleonic@gmail.com>
2024-07-10 10:23:19 -0600

    padding grid

--- day_27/tkinnter.py
+++ day_27/tkinnter.py
@@ -8,7 +8,22 @@ window.minsize(width=500, height=300)
 ny_label =  tkinter.Label(text="i am a label", font=("Arial",24,"bold"))
 ny_label.pack()
 
+ny_label["text"] = "New Text"
+ny_label.config(text="New Text")
+
+#button
+def button_clicked():
+    # ny_label["text"]= "i got clicked"
+    ny_label.config(text="Button got clicked")
+    strinput = input.get()
+    ny_label.config(text=strinput)
 
 
+button = tkinter.Button(text="Click me",command=button_clicked)
+button.pack()
+
+#entry
+input = tkinter.Entry(width=10)
+input.pack()
 
 window.mainloop()
