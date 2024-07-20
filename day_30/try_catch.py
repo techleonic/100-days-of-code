f78d9b1a53e69d35ab1d95f8915a051eb66d906d
techleonic <techleonic@gmail.com>
2024-07-19 21:54:33 -0600

    rise exception

--- day_30/try_catch.py
+++ day_30/try_catch.py
@@ -2,11 +2,17 @@
 try:
     file = open("a_file.txt")
     a_dict = {"key":"value"}
-    print(a_dict["1"])
+    # print(a_dict["1"])
+    print(a_dict["key"])
 except FileNotFoundError:
     file = open("a_file.txt","w")
     file.write("something")
-except
-    pass
+except KeyError as msg_error:
+    print(f"that key {msg_error} does not exist")
+else:
+    content = file.read()
+    print(content)
 finally:
-    pass
+    file.close()
+    print("File was closed.")
+    raise TypeError("This  is an error that i made up")
