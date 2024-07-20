eb015d3dda9c42623aa67d0402415e3978333df0
techleonic <techleonic@gmail.com>
2024-07-19 22:33:01 -0600

    nato Eror

--- day_30/main.py
+++ day_30/main.py
@@ -1,6 +1,19 @@
-dict =  {
-    "name": "leo",
-    "age":"27"
-}
-new_dict = {key:value for (key,value) in dict.items()}
-print(new_dict)
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, Only Letters int the alphabet pleas")
+    else:
+        print(output_list)
