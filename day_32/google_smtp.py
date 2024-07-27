4fa3723f50b690e9ea5c3e439d61014ba4cb9390
techleonic <techleonic@gmail.com>
2024-07-27 11:29:04 -0600

    send mail with smtplib

---
+++ day_32/google_smtp.py
@@ -0,0 +1,11 @@
+import smtplib
+
+my_email = "jlcarcamosurbina@gmail.com"
+Password = "qwtgcixnlmqlfbcb"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=Password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="techleonic@gmail.com",
+                        msg="Subject:Hello\n\nHello this a message from python")
