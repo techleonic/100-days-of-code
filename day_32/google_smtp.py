b503509c39d99395e024c834ecf35227fe6302b4
techleonic <techleonic@gmail.com>
2024-07-29 11:44:27 -0600

    datetime manipulation

--- day_32/google_smtp.py
+++ day_32/google_smtp.py
@@ -9,3 +9,13 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.sendmail(from_addr=my_email,
                         to_addrs="",
                         msg="Subject:Hello\n\nHello this a message from python")
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month =  now.month
+day_of_the_week = now.weekday()
+
+day_of_birth = dt.datetime(year=1997,month=1,day=17)
+print(day_of_birth)
