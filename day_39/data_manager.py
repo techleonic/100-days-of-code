4989317629e84e11fcc40a221d7e4bfac86dfc2f
techleonic <techleonic@gmail.com>
2024-08-31 10:18:55 -0600

    save data to google sheets

--- day_39/data_manager.py
+++ day_39/data_manager.py
@@ -1,6 +1,10 @@
+import os
+
 import requests
+APIKEY=os.getenv("APIKEY")
+END_POINT = "https://api.sheety.co/cd1a064e0f44ab9c6aca48b4624dcb9a/flightDeals/prices"
 class Fligth_storage():
-    def __init__(self,airline,type,flight_class,duration,layovers,price):
+    def __init__(self,airline,type,flight_class,duration,layovers=1,price=0):
         self.airline = airline
         self.type = type
         self.flight_class = flight_class
@@ -9,4 +13,20 @@ class Fligth_storage():
         self.price = price
 
     def save(self):
+        sheety_params = {"price":{
+            "airline": self.airline,
+            "type": self.type,
+            "class": self.flight_class,
+            "duration":self.duration,
+            "layovers": self.layovers,
+            "price": self.price}
+        }
+        head = {
+        "Authorization":APIKEY
+        }
+        result =  requests.post(url=END_POINT, json=sheety_params,headers=head)
+        print(result.text)
 
+
+# flight = Fligth_storage(airline="United picha",type="trip",flight_class="Business",duration=15.5,layovers=2,price=1500)
+# flight.save()
--- day_39/main.py
+++ day_39/main.py
@@ -1,15 +1,27 @@
 from   google_fligths import fligth_google
-
+from data_manager import Fligth_storage
 # load the json from api
 data = fligth_google("MGA","CDG",)
 flights_list = data.get_flights()
 
-#join all the fligths in a list
+#join all the flights in a list
 all_flights = flights_list["best_flights"] + flights_list["other_flights"]
 
+# for each flight
 for flight in all_flights:
-    print(flight["flights"][0]["airline"],flight["type"],flight["flights"][0]["travel_class"],round( int(flight["total_duration"])/60,2),len(flight["layovers"]),flight["price"])
+    print(len(all_flights))
+    flight_storage = Fligth_storage(airline=flight["flights"][0]["airline"],
+                                    type=flight["type"],
+                                    flight_class=flight["flights"][0]["travel_class"],
+                                    duration=round( int(flight["total_duration"])/60,2),
+                                    layovers=len(flight["layovers"]),
+                                    price=flight["price"])
+    flight_storage.save()
+    # print(flight["flights"][0]["airline"],flight["type"],flight["flights"][0]["travel_class"],round( int(flight["total_duration"])/60,2),len(flight["layovers"]),flight["price"])
+
+
+
+
 
 # result.json()["best_flights"][1]["flights"][0]["departure_airport"]
-
 # print(["best_flights"][1]["price"])
