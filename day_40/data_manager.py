a9e28c57cc5caac6ae5de1b9672cfb41bade45ae
techleonic <techleonic@gmail.com>
2024-09-04 11:29:57 -0600

    emails airline information

--- day_40/data_manager.py
+++ day_40/data_manager.py
@@ -1,10 +1,14 @@
 import os
 
 import requests
+from datetime import datetime
 APIKEY=os.getenv("FLIGHT_TOKEN")
 END_POINT = "https://api.sheety.co/cd1a064e0f44ab9c6aca48b4624dcb9a/flightDeals/prices"
+head = {
+        "Authorization":APIKEY
+        }
 class Fligth_storage():
-    def __init__(self,airline,type,flight_class,duration,departure, arrival,layovers=1,price=0):
+    def __init__(self,airline,type,flight_class,duration,departure, arrival,outbound,freturn,layovers=1,price=0,):
         self.airline = airline
         self.type = type
         self.flight_class = flight_class
@@ -13,6 +17,8 @@ class Fligth_storage():
         self.price = price
         self.departure = departure
         self.arrival = arrival
+        self.outbound = outbound
+        self.freturn = freturn
 
     def save(self):
         sheety_params = {"price":{
@@ -23,15 +29,25 @@ class Fligth_storage():
             "layovers": self.layovers,
             "price": self.price,
             "departure":self.departure,
-            "arrival":self.arrival
+            "arrival":self.arrival,
+            "outboundDate":self.outbound,
+            "returnDate":self.freturn
         }
         }
-        head = {
-        "Authorization":APIKEY
-        }
+
         result =  requests.post(url=END_POINT, json=sheety_params,headers=head)
-        print(result.text)
-
-
+def delete_old_fligths():
+    flights_list = requests.get(url=END_POINT,headers=head)
+    old_flights = flights_list.json()["prices"]
+    if not old_flights:
+        print("there is no flights in storage")
+    else:
+        for flights in old_flights:
+            deleted_results = requests.delete(url=END_POINT+f"/{flights["id"]}", headers=head)
+            print(deleted_results.text)
+def get_flights_destination(departure,arrival):
+    filter_endpoint = END_POINT+f"?filter[departure]={departure}&filter[arrival]={arrival}"
+    filter_results = requests.get(url=filter_endpoint,headers=head)
+    return filter_results.json()["prices"]
 # flight = Fligth_storage(airline="United picha",type="trip",flight_class="Business",duration=15.5,layovers=2,price=1500)
 # flight.save()
--- day_40/fligth_clup.py
+++ day_40/fligth_clup.py
@@ -1,7 +1,9 @@
 import requests
 import os
+import data_manager
 from data_manager import Fligth_storage
 from google_fligths import fligth_google
+import smtplib
 
 USER_ENDPOINT = "https://api.sheety.co/cd1a064e0f44ab9c6aca48b4624dcb9a/emails/respuestasDeFormulario1"
 USER_TOKEN = os.getenv("EMAILS_TOKEN")
@@ -15,11 +17,17 @@ results = requests.get(url=USER_ENDPOINT, headers=user_head)
 results.raise_for_status()
 list_of_users = results.json()["respuestasDeFormulario1"]
 
+#TODO: DELETE OLD FLIGTHS FROM YESTARDAY
+# data_manager.delete_old_fligths()
+
+
+# GET USERS DEPARTURE AND ARRIVAL AND POPULATES THE FLIGHTS GOOGLE SHEETS
 # list of user in the google sheet
+
 for user in list_of_users:
     fligth_search= fligth_google(departure=user["departureCode"],arrival=user["arrival"])
-    all_flights = fligth_search.get_flights()["best_flights"]
-
+    data = fligth_search.get_flights()
+    all_flights = data["best_flights"]
     # SAVE ALL FLIGHTS IN GOOGLE SHEET
     for flight in all_flights:
         try:
@@ -33,7 +41,25 @@ for user in list_of_users:
                                         layovers=layovers,
                                         price=flight["price"],
                                         departure=user["departureCode"],
-                                        arrival=user["arrival"])
+                                        arrival=user["arrival"],
+                                        outbound=data["search_parameters"]["outbound_date"],
+                                        freturn=data["search_parameters"]["return_date"]
+                                        )
         flight_storage.save()
 
-# GETS THE FLIGTHS WITH THE QUERY DEPARTURE AND ARRIVAL FROM USERS["DEPARTURE], USERS["ARRIVAL]
+# TODO: GETS THE FLIGTHS WITH THE QUERY DEPARTURE AND ARRIVAL FROM USERS["DEPARTURE], USERS["ARRIVAL]
+sender =  os.getenv("SENDER")
+password = os.getenv("PASSWORD")
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+server.login(sender, password)
+for user in list_of_users:
+    message = ''
+    text=''
+    list_filter_flights = data_manager.get_flights_destination(departure=user["departureCode"],arrival=user["arrival"])
+    for filter_flight  in list_filter_flights:
+        text+= f"Airline: {filter_flight["airline"]}\nType: {filter_flight["type"]}\nClass: {filter_flight["class"]}\nDuration: {filter_flight["duration"]}HRS\nlayovers: {filter_flight["layovers"]}\nprice: {filter_flight["price"]}$\nDeparture: {filter_flight["departure"]}\nArrival: {filter_flight["arrival"]}\n\n"
+    message = 'Subject: {}\n\n{}'.format("Airline Information", text)
+    print(message)
+    server.sendmail(sender,user["email"],message)
+server.quit()
