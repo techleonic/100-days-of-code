8e4d14206a6b8a0273496b772f574e175f204d4a
techleonic <techleonic@gmail.com>
2024-09-28 09:07:13 -0600

    post internet conexion speed

--- internet_test_automation.py
+++ internet_test_automation.py
@@ -1,7 +1,10 @@
+import os
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("disable-infobars")
@@ -15,14 +18,26 @@ chrome_options.add_experimental_option("detach", True)
 # WEB DRIVER
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get('https://www.speedtest.net/es')
+# driver.get('https://www.speedtest.net/es')
+#
+# start_btn = driver.find_element(By.CSS_SELECTOR, value='a.js-start-test.test-mode-multi')
+# start_btn.click()
+# time.sleep(60)
+# dowload_speed = driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
+# upload_speed = driver.find_element(By.XPATH, value= '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
+#
+# print(dowload_speed, upload_speed)
 
-start_btn = driver.find_element(By.CSS_SELECTOR, value='a.js-start-test.test-mode-multi')
-start_btn.click()
-time.sleep(60)
-dowload_speed = driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
-upload_speed = driver.find_element(By.XPATH, value= '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
+driver.get('https://www.facebook.com/')
+email =  driver.find_element(By.CSS_SELECTOR, value='input.inputtext._55r1._6luy')
+email.send_keys(os.getenv("EMAIL"))
+password = driver.find_element(By.CSS_SELECTOR, value='input.inputtext._55r1._6luy._9npi')
+password.send_keys(os.getenv("PASSWORD"), Keys.ENTER)
+post = driver.find_element(By.XPATH, value='//*[@id="scrollview"]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div[2]')
+post.click()
+write = driver.find_element(By.XPATH, value="/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div[1]/div/div/div[1]")
+write.send_keys("My dowload speed is: 56 \nand my upload speed is: 12")
 
-print(dowload_speed, upload_speed)
-
+post_button = driver.find_element(By.XPATH, value='/html/body/div[1]/div/div[1]/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div/div[3]/div[2]/div/div')
+post_button.click()
 driver.quit()
