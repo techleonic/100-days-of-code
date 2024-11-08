57bdb82cd01ce60a234291b488fd9cc4712fcb65
techleonic <techleonic@gmail.com>
2024-11-08 14:52:07 -0600

    add, edit , delete in shoeshop webapp

--- day_64/my_shoe_shop/shoe_shop.py
+++ day_64/my_shoe_shop/shoe_shop.py
@@ -9,8 +9,9 @@ from wtforms import StringField, SubmitField, FloatField, FileField
 from wtforms.validators import DataRequired, Length
 from flask_wtf.file import FileAllowed, FileRequired
 import requests
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect,request,url_for
 from werkzeug.utils import secure_filename
+from PIL import Image
 
 app = Flask(__name__)
 # screate key that protects forms
@@ -45,10 +46,23 @@ class addshoe(FlaskForm):
     title = StringField("Title", validators=[DataRequired(), Length(max=250)], render_kw={"placeholder":"Title product"})
     description =  StringField("Description", validators=[DataRequired(), Length(max=500)], render_kw={"class" : "Message","placeholder":"Description"})
     price = FloatField("Price", validators=[DataRequired()], render_kw={"placeholder": "Price"})
-    image = FileField("image", validators=[FileRequired(), FileAllowed(['jpg', 'jpeg', 'png'],'images only')])
+    image = FileField("image", validators=[DataRequired(), FileAllowed(['jpg', 'jpeg', 'png'],'images only')])
     submit = SubmitField("Add")
-@app.route("/", methods = ["GET","POST"])
+
+class editshoe(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(max=250)], render_kw={"placeholder":"Title product"})
+    description =  StringField("Description", validators=[DataRequired(), Length(max=500)], render_kw={"class" : "Message","placeholder":"Description"})
+    price = FloatField("Price", validators=[DataRequired()], render_kw={"placeholder": "Price"})
+    image = FileField("image", validators=[ FileAllowed(['jpg', 'jpeg', 'png'],'images only')])
+    submit = SubmitField("Edit")
+@app.route("/")
 def index():
+    result = db.session.execute(db.select(Shoe))
+    all_shoes = result.scalars().all()
+    return render_template("index.html",shoes = all_shoes)
+
+@app.route("/add", methods = ["GET","POST"])
+def add():
     form =  addshoe()
     title =form.title.data
     description = form.description.data
@@ -82,6 +96,48 @@ def shoe_table():
     all_shoes = result.scalars().all()
     db.session.commit()
     return  render_template("shoe_table.html", shoes = all_shoes)
+
+@app.route("/delete")
+def delete_item():
+    if request.method == 'GET':
+        shoe_id = request.args.get("id")
+        shoe = db.get_or_404(Shoe,shoe_id)
+        return render_template ("delete.html", shoe = shoe)
+    else:
+        request.args.get("id")
+
+@app.route("/delete_complete")
+def delete_complete():
+    shoe_id = request.args.get("id")
+    shoe =  db.get_or_404(Shoe, shoe_id)
+    db.session.delete(shoe)
+    db.session.commit()
+    return redirect(url_for("shoe_table"))
+
+@app.route("/edit", methods=["GET","POST"])
+def edit():
+    form = editshoe()
+    shoe_id = request.args.get("id")
+    shoe = db.get_or_404(Shoe, shoe_id)
+    if form.validate_on_submit():
+        shoe.title = form.title.data
+        shoe.description = form.description.data
+        shoe.price = form.price.data
+        file = form.image.data
+        if file:
+            filename = secure_filename(file.filename)
+            filepath =  os.path.join(app.config["UPLOAD_FOLDER"],filename)
+            file.save(filepath)
+            shoe.filename = f"{app.config["UPLOAD_FOLDER"]}/{filename}"
+        db.session.commit()
+        return redirect(url_for("shoe_table"))
+
+    form.title.data = shoe.title
+    form.description.data = shoe.description
+    form.price.data = shoe.price
+    return render_template("edit.html", form=form, img=shoe.filename)
+
+
 
 
 if __name__ == "__main__":
